d95c2dc77cc4a797e089f26b9322bcbe7c908ca1
rhkrrbaud <rhkrrbaudgg@naver.com>
2022-01-13 21:47:02 +0900

    새로운 컴포넌트 적용

--- src/components/Ground.js
+++ src/components/Ground.js
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { usePlane } from '@react-three/cannon';
 import { TextureLoader, RepeatWrapping } from 'three';
 import grass from '../images/grass.png';
-import { Roads } from './Roads/Roads';
+import { StartingRoad, CurvedRoad } from './Roads/RoadIndex';
 
-export const Ground = (props) => {
+const Ground = (props) => {
     const [ref] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0], ...props }));
-    const texture = new TextureLoader().load(grass);
+    const texture = useMemo(() => new TextureLoader().load(grass),[]);
     texture.wrapS = RepeatWrapping;
     texture.wrapT = RepeatWrapping;
     texture.repeat.set(100, 100);
+
     return (
         <>
-            {/* <Roads>
-
-            </Roads> */}
+            <group>
+                <StartingRoad
+                    position={[0, 0, 0]}
+                    rotation={[-Math.PI / 2, 0, 0]}
+                    args={[20, 100]}
+                />
+                <CurvedRoad
+                    position={[-45, 0, -50]}
+                    rotation={[-Math.PI / 2, 0, 0]}
+                    args={[35, 55, 15, 1, 0, Math.PI / 2]}
+                />
+            </group>
             <mesh ref={ref} receiveShadow>
-                <planeBufferGeometry attach='geometry' args={[75, 100]} />
+                <planeBufferGeometry attach='geometry' args={[175, 200]} />
                 <meshStandardMaterial map={texture} attach='material' />
             </mesh>
         </>
     );
 };
+
+export default React.memo(Ground);
