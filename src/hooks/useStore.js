aa604218e12c932933ea57db438d2addca2b1b24
rhkrrbaud <rhkrrbaudgg@naver.com>
2022-01-14 20:26:29 +0900

    light position 값을 추가

--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -24,5 +24,7 @@ export const useStore = create((set) => ({
     }),
     skyXYZ: [-100, 0, 100],
     setSkyXYZ: (x, y, z) => set(() => ({skyXYZ: [x, y, z]})),
+    lightXYZ: [-200, 0, 200],
+    setLightXYZ: (x, y, z) => set(() => ({lightXYZ: [x, y, z]})),
     
 }));
