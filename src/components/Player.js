c8f932b164331250e61cf1f0da4953029833be45
rhkrrbaud <rhkrrbaudgg@naver.com>
2022-01-14 20:20:07 +0900

    주석 처리

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react';
-import { useSphere } from '@react-three/cannon';
+import { useCompoundBody, useSphere } from '@react-three/cannon';
 import { useThree, useFrame } from '@react-three/fiber';
 import { useKeyboardControls } from '../hooks/useKeyboardControls';
 import { Vector3 } from 'three';
@@ -20,33 +20,28 @@ export const Player = (props) => {
         type: 'Dynamic',
         ...props,
       }));
-    const velocity = useRef([0, 0, 0]);
-    // useEffect(() => {
-    //     api.velocity.subscribe((v) => (velocity.current = v));
-    // }, [api,velocity])
 
-    // //   console.log(camera.getObjectByProperty())
-    // useFrame(() => {
-    //     ref.current.getWorldPosition(camera.position)
-    //     //camera.position.copy(ref.current.position);
-    //     const direction = new Vector3();
-    //     const frontVector = new Vector3(
-    //         0,
-    //         0,
-    //         moveForward ? -1 : 0 - moveBackward ? 1 : 0
-    //     );
-    //     const sideVector = new Vector3(moveLeft ? 1 : 0 - moveRight ? -1 : 0, 0, 0);
-    //     //console.log(frontVector)
-    //     //console.log(sideVector)
-    //     direction
-    //         .subVectors(frontVector, sideVector)
-    //         .normalize()
-    //         .multiplyScalar(SPEED)
-    //         .applyEuler(camera.rotation);
-    //     //console.log(direction)
-    //     api.velocity.set(direction.x, velocity.current[1], direction.z)
-    //     //console.log(camera.rotation)
-    // });
+    const velocity = useRef([0, 0, 0]);
+    useEffect(() => {
+        api.velocity.subscribe((v) => (velocity.current = v));
+    }, [api,velocity])
+
+    useFrame(() => {
+        ref.current.getWorldPosition(camera.position)
+        const direction = new Vector3();
+        const frontVector = new Vector3(
+            0,
+            0,
+            moveForward ? -1 : 0 - moveBackward ? 1 : 0
+        );
+        const sideVector = new Vector3(moveLeft ? 1 : 0 - moveRight ? -1 : 0, 0, 0);
+        direction
+            .subVectors(frontVector, sideVector)
+            .normalize()
+            .multiplyScalar(SPEED)
+            .applyEuler(camera.rotation);
+        api.velocity.set(direction.x, velocity.current[1], direction.z)
+    });
 
 
     return (
