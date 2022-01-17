e333ff1d3d78764f99977ad0a9c96f83272d827e
rhkrrbaud <rhkrrbaudgg@naver.com>
2022-01-18 00:39:20 +0900

    울타리 3d 모델 mesh를 충돌 mesh안의 하위로 집어 넣음

--- src/components/Fences/FencesIndex.js
+++ src/components/Fences/FencesIndex.js
@@ -4,27 +4,33 @@ import { useGLTF } from "@react-three/drei";
 
 export const WoodFences = ({position3d, rotation3d, scale3d, color, ...props}) => {
     const { nodes } = useGLTF("/old_fence/scene.gltf");
-
+    //console.log(nodes)
+    // nodes.Box006.rotation.set(10,20,30)
     const [ref] = useBox(() => ({
         mass: 1,
-        type: 'Static',
-        args: [23, 5, 1],
+        type: 'Dynamic',
+        args: [23, 1, 5],
+        //rotation: [-Math.PI / 2, 0, Math.PI / 2],
         ...props,
     }))
     return (
         <group dispose={null}>
             <mesh castShadow
+                ref={ref}
                 scale={scale3d}
-                rotation={rotation3d}
-                position={position3d}
-                geometry={nodes.Box006__0.geometry}
-                material={nodes.Box006__0.material}
             >
+                <mesh
+                    position={[0, 0, -500]}
+                    rotation={[0, 0, -0.1]}
+                    geometry={nodes.Box006__0.geometry}
+                    material={nodes.Box006__0.material}>
+                </mesh>
             </mesh>
-            <mesh ref={ref} castShadow>
-                {/* <boxGeometry args={[23, 5, 1]} />
-                <meshStandardMaterial color={color}/> */}
-            </mesh>
+
+            {/* <mesh ref={ref}>
+                <boxGeometry args={[23, 5, 1]} />
+                <meshStandardMaterial color={color}/>
+            </mesh> */}
         </group>
     );
 };
