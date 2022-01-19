cb0ed006788d586c24334f85b7f6ee77dc211da0
rhkrrbaud <rhkrrbaudgg@naver.com>
2022-01-19 22:54:52 +0900

    코드 수정

--- src/hooks/useKeyboardControls.js
+++ src/hooks/useKeyboardControls.js
@@ -1,59 +1,42 @@
-import { useState, useEffect } from 'react';
-import { useStore } from './useStore';
+import { useState, useEffect } from "react";
 
 function actionByKey(key) {
-    const keys = {
-        KeyW: "moveForward",
-        KeyS: "moveBackward",
-        KeyA: "moveLeft",
-        KeyD: "moveRight",
-        space: "jump",
-    };
-    return keys[key];
-}
-
-function textureByKey(key) {
-    const keys = {
-        Digit1: "dirt",
-        Digit2: "grass",
-        Digit3: "glass",
-        Digit4: "wood",
-        Digit5: "log",
-    };
-    return keys[key];
+  const keys = {
+    KeyW: "moveForward",
+    KeyS: "moveBackward",
+    KeyA: "moveLeft",
+    KeyD: "moveRight",
+    space: "jump",
+  };
+  return keys[key];
 }
 
 export const useKeyboardControls = () => {
-    const [movement, setMovement] = useState({
-        moveForward: false,
-        moveBackward: false,
-        moveLeft: false,
-        moveRight: false,
-        jump: false,
-    });
-    const setTexture = useStore((state) => state.setTexture)
+  const [movement, setMovement] = useState({
+    moveForward: false,
+    moveBackward: false,
+    moveLeft: false,
+    moveRight: false,
+    jump: false,
+  });
 
-    useEffect(() => {
-        const handleKeyDown = (e) => {
-            if (actionByKey(e.code)) {
-                setMovement((state) => ({...state, [actionByKey(e.code)]: true}))
-            }
-            if (textureByKey(e.code)) {
-                setTexture(textureByKey(e.code))
-            }
-        };
-        const handleKeyUp = (e) => {
-            if (actionByKey(e.code)) {
-                setMovement((state) => ({...state, [actionByKey(e.code)]: false}))
-            }
-        };
-        document.addEventListener('keydown', handleKeyDown);
-        document.addEventListener('keyup', handleKeyUp);
-        return () => {
-            document.addEventListener('keydown', handleKeyDown);
-            document.addEventListener('keyup', handleKeyUp);
-
-        }
-    })
-    return movement;
-}
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (actionByKey(e.code)) {
+        setMovement((state) => ({ ...state, [actionByKey(e.code)]: true }));
+      }
+    };
+    const handleKeyUp = (e) => {
+      if (actionByKey(e.code)) {
+        setMovement((state) => ({ ...state, [actionByKey(e.code)]: false }));
+      }
+    };
+    document.addEventListener("keydown", handleKeyDown);
+    document.addEventListener("keyup", handleKeyUp);
+    return () => {
+      document.addEventListener("keydown", handleKeyDown);
+      document.addEventListener("keyup", handleKeyUp);
+    };
+  });
+  return movement;
+};
