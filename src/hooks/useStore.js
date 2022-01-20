a1cd59d1db48c70b4e82d9a89dcdf13d99e7d1b9
rhkrrbaud <rhkrrbaudgg@naver.com>
2022-01-21 00:24:12 +0900

    상태 관리

--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -5,5 +5,10 @@ export const useStore = create((set) => ({
   setSkyXYZ: (x, y, z) => set(() => ({ skyXYZ: [x, y, z] })),
   lightXYZ: [-200, 50, 200],
   setLightXYZ: (x, y, z) => set(() => ({ lightXYZ: [x, y, z] })),
+  spaceShipMove: [300, 1500, 1500],
+  setSpaceShipMove: (x, y, z) => set(() => ({ spaceShipMove: [x, y, z] })),
+  spaceShipRotation: [0, 0, 0],
+  setSpaceShipRotation: (x, y, z) =>
+    set(() => ({ spaceShipRotation: [x, y, z] })),
 }));
 //lightXYZ: [-200, 0, 200], default
