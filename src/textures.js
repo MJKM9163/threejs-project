73d3a6d07a0748e167bb82535c94ee3d3d03f29f
rhkrrbaud <rhkrrbaudgg@naver.com>
2022-01-24 23:19:40 +0900

    texture 모델

--- src/textures.js
+++ src/textures.js
@@ -1,12 +1,6 @@
-import dirtImg from './images/dirt.png';
-import grassImg from './images/grass.png';
-import glassImg from './images/glass.png';
-import logImg from './images/log.png';
-import woodImg from './images/wood.png';
-import { TextureLoader } from 'three';
+import grassImg from "./images/grass.png";
+import woodImg from "./images/wood.png";
+import { TextureLoader } from "three";
 
-export const dirt = new TextureLoader().load(dirtImg)
-export const grass = new TextureLoader().load(grassImg)
-export const glass = new TextureLoader().load(glassImg)
-export const wood = new TextureLoader().load(woodImg)
-export const log = new TextureLoader().load(logImg)
+export const grass = new TextureLoader().load(grassImg);
+export const wood = new TextureLoader().load(woodImg);
