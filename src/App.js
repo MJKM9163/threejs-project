cd37ff292b13da7895d23f5b1f74a0c996c3ca36
rhkrrbaud <rhkrrbaudgg@naver.com>
2022-01-26 16:30:35 +0900

    새 작업

--- src/App.js
+++ src/App.js
@@ -1,66 +1,10 @@
-import React, { Suspense, useEffect, useRef } from "react";
+import React, { Suspense } from "react";
 import "./App.css";
-import { Canvas, useThree } from "@react-three/fiber";
-import { Html, OrbitControls, Sky, Stars, useHelper } from "@react-three/drei";
-import { Physics, Debug } from "@react-three/cannon";
-import Ground from "./components/Ground";
-import { Cube } from "./components/Cube";
-import { SetSky, SetLight, SkyCountrol } from "./controls/skyControl";
-import State from "./components/state/State";
-import { DirectionalLightHelper } from "three";
-import Character from "./components/Character";
-import { Start } from "./starting/Start";
-import styled from "styled-components";
-import { useStore } from "./hooks/useStore";
-import { Fire } from "./shaders/Fire";
-import Ocean from "./shaders/Water";
-
-const ChangeDiv = styled.div`
-  width: 100vw;
-  height: 100vh;
-  position: absolute;
-  background-color: ${(props) => (props.eventCheck ? "#494949" : "#7575750")};
-  z-index: ${(props) => (props.eventCheck ? 100 : 0)};
-  transition: 3s;
-`;
+import { Canvas } from "@react-three/fiber";
 
 function App() {
-  const Light = () => {
-    const ref = useRef();
-    useHelper(ref, DirectionalLightHelper, 10);
-
-    return (
-      <>
-        <ambientLight intensity={0.35} />
-        <directionalLight
-          ref={ref}
-          intensity={3}
-          position={SetLight()}
-          shadow-mapSize-width={2048}
-          shadow-mapSize-height={2048}
-          shadow-camera-far={1000}
-          shadow-camera-near={0.5}
-          shadow-camera-left={-300}
-          shadow-camera-right={300}
-          shadow-camera-top={-300}
-          shadow-camera-bottom={300}
-          castShadow
-        />
-      </>
-    );
-  };
-  //position: [0, 0, -2] default
-
-  const eventCheck = useStore((state) => state.eventChecker);
-  const startCheck = useStore((state) => state.startRender);
-  const render = useStore((state) => state.spaceShipRender);
-  // console.log(startCheck);
-  // console.log("App 컴포넌트 랜더링");
-  console.log("test");
   return (
     <>
-      <ChangeDiv eventCheck={eventCheck} />
-      <State />
       <Canvas
         shadows
         colorManagement
@@ -72,25 +16,7 @@ function App() {
           near: 3,
         }}
       >
-        <group>
-          {/* <SkyCountrol /> */}
-          <Sky sunPosition={SetSky()} distance={4000} turbidity={0.5} />
-        </group>
-        <Light />
-        <Physics gravity={[0, -30, 0]} step={1 / 60}>
-          <Debug scale={1}>
-            <Suspense fallback={null}>
-              <Cube position={[10, 5, 0]} type="wood" />
-              {/* <Fire scale={10} /> */}
-              <Ocean />
-              <Character />
-              {/* {startCheck ? <Start /> : <Character />} */}
-
-              <Ground position={[0, -0.1, 0]} />
-            </Suspense>
-          </Debug>
-        </Physics>
-        {/* <Stars radius={200} count={300} /> */}
+        <Suspense></Suspense>
       </Canvas>
     </>
   );
--- src/textures.js
+++
@@ -1,6 +0,0 @@
-import grassImg from "./images/grass.png";
-import woodImg from "./images/wood.png";
-import { TextureLoader } from "three";
-
-export const grass = new TextureLoader().load(grassImg);
-export const wood = new TextureLoader().load(woodImg);
