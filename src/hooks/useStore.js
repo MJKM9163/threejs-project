0f7293944d9172dd8f4f760736416835e8734d20
rhkrrbaud <rhkrrbaudgg@naver.com>
2022-01-28 21:54:29 +0900

    상태 관리

--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -1,12 +1,12 @@
 import create from "zustand";
 
-export const useStore = create((set) => ({
+export const [useStore, storeApi] = create((set) => ({
   name: "space",
   setName: (newName) => set(() => ({ name: newName })),
   zoom: false,
   setZoom: (check) => set(() => ({ zoom: check })),
   focus: {},
   setFocus: (newFocus) => set(() => ({ focus: newFocus })),
-  cameraRotationY: 0,
-  setCameraRotationY: (r) => set(() => ({ cameraRotationY: r })),
+  orbitHide: false,
+  setOrbitHide: (check) => set(() => ({ orbitHide: check })),
 }));
