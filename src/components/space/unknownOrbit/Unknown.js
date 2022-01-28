603c269f2bf61dc54849446a04a6ad2c50595793
rhkrrbaud <rhkrrbaudgg@naver.com>
2022-01-28 21:54:08 +0900

    unknown

--- src/components/space/unknownOrbit/Unknown.js
+++ src/components/space/unknownOrbit/Unknown.js
@@ -1,27 +1,49 @@
 import { useSphere } from "@react-three/cannon";
-import React from "react";
+import { useFrame } from "@react-three/fiber";
+import React, { useEffect, useRef } from "react";
+import { Vector3 } from "three";
+import { storeApi, useStore } from "../../../hooks/useStore";
+import { OrbitLine } from "../OrbitLine";
 
-export const Unknown = ({ ...props }) => {
-  const [earthRef, earthApi] = useSphere(() => ({
+let a = 0;
+
+export const Unknown = ({ focusAndView, ...props }) => {
+  const unknownWorldPosition = new Vector3();
+  const [unknownRef, unknownApi] = useSphere(() => ({
     mass: 1,
     type: "Static",
     position: props.position,
-    //rotation: [10, 10, 10],
     args: [27],
   }));
-  console.log("unknown");
+
+  const orbitHide = useRef(useStore.getState().orbitHide);
+  const hideCheck = useStore((state) => state.setOrbitHide);
+
+  useEffect(() => {
+    storeApi.subscribe((state) => {
+      orbitHide.current = state.orbitHide;
+    });
+  });
+
+  useFrame(() => {
+    unknownApi.rotation.set(0, (a += 0.01), 0);
+    unknownRef.current?.getWorldPosition(unknownWorldPosition);
+  });
+
+  console.log("unknown 랜더링 확인");
   return (
     <>
-      <mesh ref={earthRef}>
+      <mesh
+        ref={unknownRef}
+        onClick={(e) => {
+          focusAndView(unknownWorldPosition, "unknown");
+          hideCheck(!orbitHide.current);
+        }}
+      >
         <sphereGeometry args={[27]} />
         <meshNormalMaterial />
       </mesh>
-      <mesh rotation={[-Math.PI / 2, 0, 0]}>
-        <ringGeometry
-          args={[props.position[0] - 5, props.position[0] + 5, 100]}
-        />
-        <meshNormalMaterial />
-      </mesh>
+      <OrbitLine args={[props.position[0] - 5, props.position[0] + 5, 100]} />
     </>
   );
 };
