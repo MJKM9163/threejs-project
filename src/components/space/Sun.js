dd58c8f31881b6dbb84c6bfd20d22fd7d41a8eb1
rhkrrbaud <rhkrrbaudgg@naver.com>
2022-01-28 21:51:52 +0900

    중심 행성

--- src/components/space/Sun.js
+++ src/components/space/Sun.js
@@ -1,13 +1,11 @@
 import { useSphere } from "@react-three/cannon";
 import { useFrame } from "@react-three/fiber";
-import React, { useEffect, useState, useMemo } from "react";
-import { useStore } from "../../hooks/useStore";
+import React, { useEffect, useRef } from "react";
+import { storeApi, useStore } from "../../hooks/useStore";
 
 let a = 0.5;
 
 export const Sun = ({ focusAndView }) => {
-  const setZoom = useStore((state) => state.setZoom);
-  //const [tt, setTT] = useState(false);
   const [sunRef, sunApi] = useSphere(() => ({
     mass: 100,
     type: "Static",
@@ -16,10 +14,19 @@ export const Sun = ({ focusAndView }) => {
     args: [100],
   }));
 
-  console.log("sun component");
+  const orbitHide = useRef(useStore.getState().orbitHide);
+  const hideCheck = useStore((state) => state.setOrbitHide);
+
+  useEffect(() => {
+    storeApi.subscribe((state) => {
+      orbitHide.current = state.orbitHide;
+    });
+  });
+
   useFrame(() => {
     sunApi.rotation.set(0, (a += 0.01), 0);
   });
+  console.log("태양 랜더링 확인");
 
   return (
     <>
@@ -27,7 +34,7 @@ export const Sun = ({ focusAndView }) => {
         ref={sunRef}
         onClick={(e) => {
           focusAndView(e.object.position, "sun");
-          setZoom(true);
+          hideCheck(!orbitHide.current);
         }}
       >
         <sphereGeometry args={[100]} />
