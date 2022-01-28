467d29660cdc44c697822820955b01969426935c
rhkrrbaud <rhkrrbaudgg@naver.com>
2022-01-28 21:53:07 +0900

    eartch

--- src/components/space/earthOrbit/Earth.js
+++ src/components/space/earthOrbit/Earth.js
@@ -1,46 +1,49 @@
 import { useSphere } from "@react-three/cannon";
 import { useFrame } from "@react-three/fiber";
-import React, { useEffect, useState } from "react";
-import { useStore } from "../../../hooks/useStore";
+import React, { useEffect, useRef } from "react";
+import { Vector3 } from "three";
+import { storeApi, useStore } from "../../../hooks/useStore";
+import { OrbitLine } from "../OrbitLine";
 
 let a = 0;
-let aa = 0;
 
 export const Earth = ({ focusAndView, ...props }) => {
-  //const zoom = useStore((state) => state.zoom);
-  const setZoom = useStore((state) => state.setZoom);
+  const earthWorldPosition = new Vector3();
   const [earthRef, earthApi] = useSphere(() => ({
     mass: 1,
     type: "Static",
     position: props.position,
-    //rotation: [10, 10, 10],
     args: [20],
   }));
-  //earthApi.rotation.subscribe((p) => console.log(p));
+
+  let orbitHide = useRef(useStore.getState().orbitHide);
+  let hideCheck = useStore((state) => state.setOrbitHide);
+
+  useEffect(() => {
+    storeApi.subscribe((state) => {
+      orbitHide.current = state.orbitHide;
+    });
+  });
 
   useFrame(() => {
     earthApi.rotation.set(0, (a += 0.01), 0);
+    earthRef.current?.getWorldPosition(earthWorldPosition);
   });
 
+  console.log("earth 랜더링 확인");
   return (
     <>
       <mesh
         ref={earthRef}
         onClick={(e) => {
-          focusAndView(e.object.position, "earth");
-          setZoom(true);
+          focusAndView(earthWorldPosition, "earth");
+          hideCheck(!orbitHide.current);
         }}
       >
-        {/* <sphereGeometry args={[20]} /> */}
         <boxGeometry args={[100, 100, 100]} />
         <meshNormalMaterial />
       </mesh>
-      <mesh rotation={[-Math.PI / 2, 0, 0]}>
-        <ringGeometry
-          args={[props.position[0] - 5, props.position[0] + 5, 100]}
-        />
-        <meshNormalMaterial />
-      </mesh>
+      <OrbitLine args={[props.position[0] - 5, props.position[0] + 5, 100]} />
     </>
   );
 };
