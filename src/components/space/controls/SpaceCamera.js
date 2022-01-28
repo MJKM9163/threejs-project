699ffad8f1454057491c40dc494eeaac1ed48a5b
rhkrrbaud <rhkrrbaudgg@naver.com>
2022-01-28 21:52:31 +0900

    space Camera

--- src/components/space/controls/SpaceCamera.js
+++ src/components/space/controls/SpaceCamera.js
@@ -1,34 +1,41 @@
-import React, { useMemo } from "react";
+import React, { useEffect, useMemo, useRef } from "react";
 import CameraControls from "camera-controls";
 import * as THREE from "three";
 import { Vector3 } from "three";
 import { useFrame, useThree } from "@react-three/fiber";
-import { useStore } from "../../../hooks/useStore";
+import { storeApi, useStore } from "../../../hooks/useStore";
 
 CameraControls.install({ THREE: THREE });
 //state.cameraRotationY
 export const SpaceCamera = ({ pos = new Vector3(), look = new Vector3() }) => {
-  const obj = {
-    space: "112222",
-    earth: "32323",
-  };
+  console.log("카메라 랜더링 확인");
 
-  const name = useStore((state) => state.name);
-  const zoom = useStore((state) => state.zoom);
-  const focus = useStore((state) => state.focus);
-  const cameraRotationY = useStore((state) => null);
-  console.log(name);
-  console.log(obj[name]);
+  let focus = useRef(useStore.getState().focus);
+  let zoomCheck = useRef(useStore.getState().zoom);
+  useEffect(() => {
+    storeApi.subscribe((state) => {
+      focus.current = state.focus;
+    });
+    storeApi.subscribe((state) => {
+      zoomCheck.current = state.zoom;
+    });
+  });
 
   const camera = useThree((state) => state.camera);
   const gl = useThree((state) => state.gl);
   const controls = useMemo(() => new CameraControls(camera, gl.domElement), []);
 
   return useFrame((state, delta) => {
-    zoom
-      ? pos.set(focus.x + 400, focus.y + 550, focus.z + -500)
+    zoomCheck.current
+      ? pos.set(
+          focus.current.x + 400,
+          focus.current.y + 550,
+          focus.current.z + -500
+        )
       : pos.set(1100, 2600, -4000);
-    zoom ? look.set(focus.x, focus.y, focus.z) : look.set(0, 0, 0);
+    zoomCheck.current
+      ? look.set(focus.current.x, focus.current.y, focus.current.z)
+      : look.set(0, 0, 0);
 
     state.camera.position.lerp(pos, 0.1);
     state.camera.updateProjectionMatrix();
