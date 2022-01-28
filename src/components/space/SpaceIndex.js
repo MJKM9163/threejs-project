dcf50d916fb2f30c58d2536aed132894f7c64f79
rhkrrbaud <rhkrrbaudgg@naver.com>
2022-01-28 21:51:13 +0900

    space component

--- src/components/space/SpaceIndex.js
+++ src/components/space/SpaceIndex.js
@@ -1,45 +1,58 @@
-import { useCylinder, useSphere } from "@react-three/cannon";
 import { useFrame } from "@react-three/fiber";
 import React, { useEffect, useRef, useState } from "react";
-import { useStore } from "../../hooks/useStore";
+import { useStore, storeApi } from "../../hooks/useStore";
 import { SpaceCamera } from "./controls/SpaceCamera";
 import { Earth } from "./earthOrbit/Earth";
 import { Sun } from "./Sun";
 import { Unknown } from "./unknownOrbit/Unknown";
 
 let earthStarting = 0.0;
-let unknownStarting = 10.0;
+let unknownStarting = 0.0;
 export const SpaceIndex = () => {
+  const setZoom = useStore((state) => state.setZoom);
   const setFocus = useStore((state) => state.setFocus);
   const viewTarget = useStore((state) => state.setName);
-  const setCameraRotationY = useStore((state) => state.setCameraRotationY);
 
   const allOrbitRef = useRef();
   const earthOrbitRef = useRef();
-
   const unknownOrbitRef = useRef();
-  console.log(earthOrbitRef.current?.rotation);
+
+  let zoomCheck = useRef(useStore.getState().zoom);
+  useEffect(() => {
+    storeApi.subscribe((state) => {
+      zoomCheck.current = state.zoom;
+    });
+  });
+
   useFrame(() => {
     earthOrbitRef.current?.rotation.set(0, (earthStarting += 0.0015), 0);
     unknownOrbitRef.current?.rotation.set(0, (unknownStarting -= 0.0005), 0);
-    //console.log(earthOrbitRef.current?.rotation.y);
-    setCameraRotationY(earthOrbitRef.current?.rotation.y);
   });
 
+  console.log("우주 랜더링 확인");
   return (
     <>
       <group ref={allOrbitRef} rotation={[0, 0, 0]}>
         <Sun
-          focusAndView={(focus, name) => (setFocus(focus), viewTarget(name))}
+          focusAndView={(focus, name) => (
+            setFocus(focus), viewTarget(name), setZoom(!zoomCheck.current)
+          )}
         />
         <group ref={earthOrbitRef} rotation={[0, 0, 0]}>
           <Earth
             position={[550, 0, 0]}
-            focusAndView={(focus, name) => (setFocus(focus), viewTarget(name))}
+            focusAndView={(focus, name) => (
+              setFocus(focus), viewTarget(name), setZoom(!zoomCheck.current)
+            )}
           />
         </group>
         <group ref={unknownOrbitRef}>
-          <Unknown position={[1200, 0, 0]} />
+          <Unknown
+            position={[1200, 0, 0]}
+            focusAndView={(focus, name) => (
+              setFocus(focus), viewTarget(name), setZoom(!zoomCheck.current)
+            )}
+          />
         </group>
         <axesHelper scale={500} />
         <SpaceCamera />
