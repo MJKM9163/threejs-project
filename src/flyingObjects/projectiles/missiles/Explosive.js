e3a7bb5e93b60467609971c40ec7bf2624db45e9
rhkrrbaud <rhkrrbaudgg@naver.com>
2022-03-12 04:32:03 +0900

    미사일 컴포넌트

--- src/flyingObjects/projectiles/missiles/Explosive.js
+++ src/flyingObjects/projectiles/missiles/Explosive.js
@@ -2,38 +2,30 @@ import React, { memo, useRef } from "react";
 import { useGLTF } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { useBox } from "@react-three/cannon";
-import { boundingStore } from "../../../hooks/stores/boundingStore";
 
-export const Explosive = ({ controll, api, pos, num }) => {
-  //const explosiveNum = boundingStore((state) => state.explosiveNum);
-  const explosiveNum = boundingStore.getState().explosiveNum;
+export const Explosive = ({ mPos, tPos, num, pos }) => {
   const { nodes, materials } = useGLTF("flyingObjects/projectiles/explosive/scene.gltf");
-
+  console.log(pos);
   const [ref, missilesApi] = useBox(() => ({
     type: "Dynamic",
     mass: 1,
-    position: [0, 2000, 0],
-    args: [10, 10, 700],
+    position: [...Object.values(pos.MPos)],
+    args: [100, 100, 700],
   }));
 
   let moveFun = () => {
-    let xPos = (explosiveNum[num].MPos.x - explosiveNum[num].TPos.x) * -1;
-    let zPos = (explosiveNum[num].MPos.z - explosiveNum[num].TPos.z) * -1;
+    let xPos = (mPos.x - tPos.x) * 1;
+    let zPos = (mPos.z - tPos.z) * 1;
 
     return [xPos, zPos];
   };
 
-  useFrame(() => {});
-  //   console.log("각 미사일");
-  // });
-  // useFrame(({ clock }) => {
-  //   // clock.autoStart = false;
-  //   let [X, Z] = moveFun();
-  //   missilesApi.velocity.set(X / 2, 0, Z / 2);
-  //   //console.log(clock);
-  // });
+  useFrame(() => {
+    let [X, Z] = moveFun();
+    missilesApi.velocity.set(X / 2, 0, Z / 2);
+  });
   console.log("미사일");
-  // ref={ref}
+
   return (
     <group ref={ref} dispose={null}>
       <group position={[-200, 0, 0]} rotation={[-Math.PI / 2, 0, 0]}>
