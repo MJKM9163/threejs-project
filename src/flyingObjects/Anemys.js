d63619596e74a7a6382480d3480e94bb0d683a6b
rhkrrbaud <rhkrrbaudgg@naver.com>
2022-03-12 04:30:11 +0900

    적 비행기

--- src/flyingObjects/Anemys.js
+++ src/flyingObjects/Anemys.js
@@ -5,18 +5,16 @@ import { EnemyFighter, MemoEnemyFighter } from "./fighters/enemyFighter";
 
 export const Anemys = () => {
   const num = useRef();
-  //const enemyNum = boundingStore.getState().enemyNum;
   const enemyNum = boundingStore((state) => state.enemyNum);
 
-  console.log("적 비행기 컴포넌트");
   return (
     <group ref={num}>
       {enemyNum.map((item, index) => (
-        <MemoEnemyFighter
-          key={item + index}
+        <EnemyFighter
+          key={index}
           num={index + 1}
-          position={[1800 + index * -1800, 0, -3500]}
-          mPos={[1800 + index * -1800 + 500, 0, -3500]}
+          position={[1800, 0, -3500 + index * -1800]}
+          mPos={[1800, 500, -3500 + index * -1800]}
           rotation={[0, 0, 0]}
           args={300}
           scale={25}
