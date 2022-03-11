c1eb09b6a250c69b703c7f5e2dd663db3ab28a2f
rhkrrbaud <rhkrrbaudgg@naver.com>
2022-03-12 04:32:29 +0900

    boundingSphere 모음 store

--- src/hooks/stores/boundingStore.js
+++ src/hooks/stores/boundingStore.js
@@ -4,16 +4,18 @@ import { subscribeWithSelector } from "zustand/middleware";
 
 export const boundingStore = create(
   subscribeWithSelector((set) => ({
+    // 감지 BS 저장
     fighter: {
       friendly: {},
       enemy: {},
     },
-    planet: {},
+    //planet: {},
 
-    enemyNum: [1],
-    friendlyNum: [1],
-    explosiveNum: [{ MPos: new Vector3(0, 2000, 0), TPos: new Vector3(1000, 0, 0) }],
-    pulseNum: [1],
+    enemyNum: [], // 적군 비행기 수
+    friendlyNum: [], // 아군 비행기 수
+
+    //explosiveNum: [{ MPos: new Vector3(0, 2000, 0), TPos: new Vector3(1000, 0, 0) }],
+    //pulseNum: [1],
   }))
 );
 //{ MPos: new Vector3(0, 2000, 0), TPos: new Vector3(1000, 0, 0) }
