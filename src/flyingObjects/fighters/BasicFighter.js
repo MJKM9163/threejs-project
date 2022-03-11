f794447b19ec5eaaf0f1cf0b166d6bd3846f2602
rhkrrbaud <rhkrrbaudgg@naver.com>
2022-03-12 04:31:34 +0900

    전투기

--- src/flyingObjects/fighters/BasicFighter.js
+++ src/flyingObjects/fighters/BasicFighter.js
@@ -1,20 +1,24 @@
 import React, { useEffect, useRef } from "react";
 import { useGLTF } from "@react-three/drei";
-import { useSphere } from "@react-three/cannon";
+import { useBox, useSphere } from "@react-three/cannon";
 import { useFrame, useThree } from "@react-three/fiber";
 import { effectSound } from "../../hooks/stores/effectSound";
 import { boundingStore } from "../../hooks/stores/boundingStore";
+import { Vector3 } from "three";
 
-export const BasicFighter = ({ args, position, rotation, num }) => {
+export const BasicFighter = ({ args, position, mPos, rotation, num }) => {
   let launch = false;
+  let MTime;
+  let a = 0;
   const move = useRef();
   const BS = useRef();
   const fighter = boundingStore.getState().fighter;
   const { nodes, materials } = useGLTF("flyingObjects/basicFighter/scene.gltf");
+  const missileModel = useGLTF("flyingObjects/projectiles/explosive/scene.gltf");
   const { raycaster, scene, clock } = useThree();
 
   const [collideRef, collideApi] = useSphere(() => ({
-    type: "Static",
+    type: "Dynamic",
     mass: 100,
     position,
     rotation,
@@ -24,8 +28,22 @@ export const BasicFighter = ({ args, position, rotation, num }) => {
       //console.log("아군 비행기 충돌!");
       //collideApi.position.set();
     },
-    onCollideEnd: () => {
-      //console.log("충돌 끝");
+  }));
+
+  const [missileRef, missilesApi] = useBox(() => ({
+    type: "Dynamic",
+    mass: 1,
+    position: mPos,
+    rotation: [0, -Math.PI / 2, 0],
+    args: [10, 10, 50],
+    onCollide: (e) => {
+      clearInterval(MTime);
+      launch = false;
+      let pos = new Vector3(0, 500, 0);
+      pos.add(BS.current.geometry.boundingSphere.center);
+      //effectSound.getState().fighter.FlightExplosionSound.action();
+      missilesApi.position.set(...Object.values(pos));
+      missilesApi.rotation.set(0, -Math.PI / 2, 0);
     },
   }));
 
@@ -39,76 +57,134 @@ export const BasicFighter = ({ args, position, rotation, num }) => {
     }
   };
 
+  let moveFun = () => {
+    let xPos;
+    let yPos;
+    let zPos;
+    let boundingArray = boundingStore.getState().fighter.enemy;
+    for (let key in boundingArray) {
+      const check = BS.current.geometry.boundingSphere?.intersectsSphere(boundingArray[key]);
+      if (check === true) {
+        xPos = (BS.current.geometry.boundingSphere.center.x - boundingArray[key].center.x) * -1;
+        yPos = (missileRef.current.position.y - boundingArray[key].center.y) * -1;
+        zPos = (BS.current.geometry.boundingSphere.center.z - boundingArray[key].center.z) * -1;
+
+        return [xPos, yPos, zPos];
+      }
+    }
+  };
+
   useFrame(() => {
-    //collideApi.velocity.set(0, 0, 2000);
-    //collideApi.velocity.set(Math.sin(clock.getElapsedTime() * 2) * 5000, 0, 0);
+    //collideApi.velocity.set(0, 0, Math.sin(clock.getElapsedTime() * 1) * 1800);
     if (BS.current.geometry.boundingSphere) {
       collideRef.current.getWorldPosition(BS.current.geometry.boundingSphere.center);
+      collideRef.current.getWorldPosition(move.current.geometry.boundingSphere.center);
       boundingStore.getState().fighter.friendly = {
         ...fighter.friendly,
-        ["전투기" + num]: BS.current.geometry.boundingSphere,
+        ["전투기" + num]: move.current.geometry.boundingSphere,
       };
-    }
+      if (boundingDetect()) {
+        let [X, Y, Z] = moveFun();
+        collideApi.velocity.set(X / 10, 0, Z / 10);
+        missilesApi.velocity.set(X * 3.5, Y * 3.5, Z * 3.5);
 
-    if (boundingDetect()) {
-      move.current.material.color.set("yellow");
-    } else {
-      move.current.material.color.set("blue");
-    }
+        BS.current.material.color.set("yellow");
 
-    // if (launch === false) {
-    //   launch = true;
-    //   const add = boundingStore.getState().friendlyNum;
-    //   //boundingStore.setState({ friendlyNum: [...add, 3] });
-    //   setTimeout(() => {
-    //     launch = false;
-    //   }, 3000);
-    // }
+        if (launch === false) {
+          MTime = setInterval(() => {
+            a += 1;
+            console.log("인터벌 타임 실행 중...");
+          }, 1000);
+          launch = true;
+        }
+        if (a === 3) {
+          clearInterval(MTime);
+          a = 0;
+          launch = false;
+          missilesApi.position.set(
+            ...collideRef.current.getWorldPosition(BS.current.geometry.boundingSphere.center).add(new Vector3(0, 500, 0))
+          );
+        }
+      } else {
+        //collideApi.velocity.set(0, 0, Math.sin(clock.getElapsedTime() * 1) * 500);
+        //collideApi.velocity.set(0, 0, 0);.
+        missilesApi.position.set(
+          ...collideRef.current.getWorldPosition(BS.current.geometry.boundingSphere.center).add(new Vector3(0, 500, 0))
+        );
+        BS.current.material.color.set("blue");
+      }
+    }
   });
 
   console.log("비행체");
   return (
-    <group ref={collideRef} dispose={null}>
-      <mesh ref={move}>
-        <sphereGeometry args={[args + 50]} />
-        <meshStandardMaterial wireframe opacity={0.5} transparent color={"green"} />
-      </mesh>
-      <mesh ref={BS}>
-        <sphereGeometry args={[args + 50]} />
-        <meshStandardMaterial wireframe opacity={0} transparent />
-      </mesh>
-      <group rotation={[-Math.PI / 2, 0, Math.PI / 2]} scale={20}>
-        <mesh geometry={nodes["wave-material"].geometry} material={nodes["wave-material"].material} />
-        <group scale={0.75}>
-          <mesh geometry={nodes["wave-material_1"].geometry} material={nodes["wave-material_1"].material} />
+    <group>
+      <group ref={collideRef} dispose={null}>
+        <mesh ref={move}>
+          <sphereGeometry args={[args + 100]} />
+          <meshStandardMaterial wireframe opacity={0.1} transparent />
+        </mesh>
+        <mesh ref={BS}>
+          <sphereGeometry args={[args + 2500]} />
+          <meshStandardMaterial wireframe opacity={0.2} transparent />
+        </mesh>
+        <group rotation={[-Math.PI / 2, 0, Math.PI / 2]} scale={20}>
+          <mesh geometry={nodes["wave-material"].geometry} material={nodes["wave-material"].material} />
+          <group scale={0.75}>
+            <mesh geometry={nodes["wave-material_1"].geometry} material={nodes["wave-material_1"].material} />
+          </group>
+          <group scale={0.75}>
+            <mesh geometry={nodes["wave-material_2"].geometry} material={nodes["wave-material_2"].material} />
+          </group>
+          <group scale={0.75}>
+            <mesh geometry={nodes["wave-material_3"].geometry} material={nodes["wave-material_3"].material} />
+          </group>
+          <group position={[-2.66, -3.49, -1.8]} rotation={[0, Math.PI / 6, Math.PI]} scale={[0.75, 0.75, 0.75]}>
+            <mesh geometry={nodes["landing-gear-material"].geometry} material={nodes["landing-gear-material"].material} />
+          </group>
+          <group position={[-2.66, -3.49, -1.87]} rotation={[0, Math.PI / 6, 0]} scale={[0.75, 0.75, 0.75]}>
+            <mesh
+              geometry={nodes["landing-gear-material_1"].geometry}
+              material={nodes["landing-gear-material_1"].material}
+            />
+          </group>
+          <group position={[0, -0.94, -2.09]} scale={0.75}>
+            <mesh
+              geometry={nodes["landing-gear-material_2"].geometry}
+              material={nodes["landing-gear-material_2"].material}
+            />
+          </group>
+          <group position={[2.65, -3.49, -1.8]} rotation={[0, -Math.PI / 6, 0]} scale={[0.75, 0.75, 0.75]}>
+            <mesh
+              geometry={nodes["landing-gear-material_3"].geometry}
+              material={nodes["landing-gear-material_3"].material}
+            />
+          </group>
+          <group position={[2.65, -3.49, -1.87]} rotation={[0, -Math.PI / 6, 0]} scale={[0.75, 0.75, 0.75]}>
+            <mesh
+              geometry={nodes["landing-gear-material_4"].geometry}
+              material={nodes["landing-gear-material_4"].material}
+            />
+          </group>
+          <mesh geometry={nodes["glow-material"].geometry} material={materials.glow} />
+          <group position={[-6.59, -9.24, 0.97]} rotation={[0, 0.55, 0]}>
+            <mesh geometry={nodes.Object_23.geometry} material={nodes.Object_23.material} />
+          </group>
+          <group position={[6.59, -9.24, 0.97]} rotation={[0, -0.55, 0]}>
+            <mesh geometry={nodes.Object_25.geometry} material={nodes.Object_25.material} />
+          </group>
         </group>
-        <group scale={0.75}>
-          <mesh geometry={nodes["wave-material_2"].geometry} material={nodes["wave-material_2"].material} />
-        </group>
-        <group scale={0.75}>
-          <mesh geometry={nodes["wave-material_3"].geometry} material={nodes["wave-material_3"].material} />
-        </group>
-        <group position={[-2.66, -3.49, -1.8]} rotation={[0, Math.PI / 6, Math.PI]} scale={[0.75, 0.75, 0.75]}>
-          <mesh geometry={nodes["landing-gear-material"].geometry} material={nodes["landing-gear-material"].material} />
-        </group>
-        <group position={[-2.66, -3.49, -1.87]} rotation={[0, Math.PI / 6, 0]} scale={[0.75, 0.75, 0.75]}>
-          <mesh geometry={nodes["landing-gear-material_1"].geometry} material={nodes["landing-gear-material_1"].material} />
-        </group>
-        <group position={[0, -0.94, -2.09]} scale={0.75}>
-          <mesh geometry={nodes["landing-gear-material_2"].geometry} material={nodes["landing-gear-material_2"].material} />
-        </group>
-        <group position={[2.65, -3.49, -1.8]} rotation={[0, -Math.PI / 6, 0]} scale={[0.75, 0.75, 0.75]}>
-          <mesh geometry={nodes["landing-gear-material_3"].geometry} material={nodes["landing-gear-material_3"].material} />
-        </group>
-        <group position={[2.65, -3.49, -1.87]} rotation={[0, -Math.PI / 6, 0]} scale={[0.75, 0.75, 0.75]}>
-          <mesh geometry={nodes["landing-gear-material_4"].geometry} material={nodes["landing-gear-material_4"].material} />
-        </group>
-        <mesh geometry={nodes["glow-material"].geometry} material={materials.glow} />
-        <group position={[-6.59, -9.24, 0.97]} rotation={[0, 0.55, 0]}>
-          <mesh geometry={nodes.Object_23.geometry} material={nodes.Object_23.material} />
-        </group>
-        <group position={[6.59, -9.24, 0.97]} rotation={[0, -0.55, 0]}>
-          <mesh geometry={nodes.Object_25.geometry} material={nodes.Object_25.material} />
+      </group>
+      <group ref={missileRef} dispose={null}>
+        <group position={[-200, 0, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+          <group position={[387.34, -175.97, 15.53]} rotation={[1.47, 0.76, 0.07]} />
+          <mesh geometry={missileModel.nodes.Material2.geometry} material={missileModel.materials.auto_13} />
+          <mesh geometry={missileModel.nodes.Material2_1.geometry} material={missileModel.materials.auto_14} />
+          <mesh geometry={missileModel.nodes.Material2_2.geometry} material={missileModel.materials.auto_16} />
+          <lineSegments
+            geometry={missileModel.nodes.Material2_3.geometry}
+            material={missileModel.nodes.Material2_3.material}
+          />
         </group>
       </group>
     </group>
--- src/flyingObjects/fighters/enemyFighter.js
+++ src/flyingObjects/fighters/enemyFighter.js
@@ -1,20 +1,21 @@
-import React, { memo, useEffect, useRef } from "react";
+import React, { memo, useEffect, useRef, useState } from "react";
 import { useGLTF } from "@react-three/drei";
 import { useBox, useSphere } from "@react-three/cannon";
 import { useFrame, useThree } from "@react-three/fiber";
 import { effectSound } from "../../hooks/stores/effectSound";
 import { boundingStore } from "../../hooks/stores/boundingStore";
-import { Vector3 } from "three";
+import { Euler, Vector3 } from "three";
 
-let a = 0;
 export const EnemyFighter = ({ args, position, mPos, rotation, num }) => {
   let launch = false;
+  let MTime;
+  let a = 0;
   const move = useRef();
   const BS = useRef();
   const { clock } = useThree();
   const fighter = boundingStore.getState().fighter;
   const { nodes, materials } = useGLTF("flyingObjects/enemyFighter/scene.gltf");
-  //const { nodes, materials } = useGLTF("flyingObjects/projectiles/explosive/scene.gltf");
+  const missileModel = useGLTF("flyingObjects/projectiles/explosive/scene.gltf");
 
   const [collideRef, collideApi] = useSphere(() => ({
     type: "Dynamic",
@@ -24,6 +25,23 @@ export const EnemyFighter = ({ args, position, mPos, rotation, num }) => {
     args: [args],
     onCollide: (e) => {
       //effectSound.getState().fighter.FlightExplosionSound.action();
+    },
+  }));
+
+  const [missileRef, missilesApi] = useBox(() => ({
+    type: "Dynamic",
+    mass: 1,
+    position: mPos,
+    rotation: [0, -Math.PI / 2, 0],
+    args: [10, 10, 50],
+    onCollide: (e) => {
+      clearInterval(MTime);
+      launch = false;
+      let pos = new Vector3(0, 500, 0);
+      pos.add(BS.current.geometry.boundingSphere.center);
+      //effectSound.getState().fighter.FlightExplosionSound.action();
+      missilesApi.position.set(...Object.values(pos));
+      missilesApi.rotation.set(0, -Math.PI / 2, 0);
     },
   }));
 
@@ -41,71 +59,74 @@ export const EnemyFighter = ({ args, position, mPos, rotation, num }) => {
 
   let moveFun = () => {
     let xPos;
+    let yPos;
     let zPos;
     let boundingArray = boundingStore.getState().fighter.friendly;
     for (let key in boundingArray) {
       const check = BS.current.geometry.boundingSphere?.intersectsSphere(boundingArray[key]);
       if (check === true) {
         xPos = (BS.current.geometry.boundingSphere.center.x - boundingArray[key].center.x) * -1;
+        yPos = (missileRef.current.position.y - boundingArray[key].center.y) * -1;
         zPos = (BS.current.geometry.boundingSphere.center.z - boundingArray[key].center.z) * -1;
 
-        return [xPos, zPos];
+        return [xPos, yPos, zPos];
       }
     }
   };
 
   useFrame(() => {
-    //collideApi.velocity.set(Math.sin(clock.getElapsedTime() * 1) * -1700, 0, 0); // 가속!
+    //collideApi.velocity.set(Math.sin(clock.getElapsedTime() * 2) * -3500, 0, 0); // 가속!
+    missilesApi.rotation.set(0, -Math.PI / 2, 0);
     if (BS.current.geometry.boundingSphere) {
       collideRef.current.getWorldPosition(BS.current.geometry.boundingSphere.center);
-      boundingStore.getState().fighter.enemy = { ...fighter.enemy, ["전투기" + num]: BS.current.geometry.boundingSphere };
-    }
-    if (boundingDetect()) {
-      let [X, Z] = moveFun();
-      collideApi.velocity.set(X / 10, 0, Z / 10);
-      move.current.material.color.set("yellow");
-      if (launch === false) {
-        console.log("들어옴");
-        launch = true;
-        const MPos = new Vector3();
-        const plus = new Vector3(0, 1500, 0);
-        const TPos = new Vector3(X, 0, Z);
-        //const TPos = new Vector3(-1000, 0, 0);
-        collideRef.current.getWorldPosition(MPos);
-        MPos.add(plus);
-        const PosArray = { MPos: MPos, TPos: TPos };
-        const add = boundingStore.getState().explosiveNum;
-        boundingStore.setState({ explosiveNum: [...add, PosArray] });
+      collideRef.current.getWorldPosition(move.current.geometry.boundingSphere.center);
 
-        // const test = boundingStore.getState().enemyNum;
-        // boundingStore.getState().enemyNum = [...test, 3];
-        // console.log(test);
+      boundingStore.getState().fighter.enemy = { ...fighter.enemy, ["전투기" + num]: move.current.geometry.boundingSphere };
+      if (boundingDetect()) {
+        let [X, Y, Z] = moveFun();
+        collideApi.velocity.set(X / 10, 0, Z / 10);
+        missilesApi.velocity.set(X * 3.5, Y * 3.5, Z * 3.5);
 
-        //boundingStore.setState({ enemyNum: [...add, PosArray] });
-        //boundingStore.getState().explosiveNum = [...add, PosArray];
+        BS.current.material.color.set("yellow");
 
-        setTimeout(() => {
+        if (launch === false) {
+          MTime = setInterval(() => {
+            a += 1;
+            console.log("인터벌 타임 실행 중...");
+          }, 1000);
+          launch = true;
+        }
+        if (a === 3) {
+          clearInterval(MTime);
+          a = 0;
           launch = false;
-        }, 3000);
+          missilesApi.position.set(
+            ...collideRef.current.getWorldPosition(BS.current.geometry.boundingSphere.center).add(new Vector3(0, 500, 0))
+          );
+        }
+      } else {
+        //collideApi.velocity.set(0, 0, Math.sin(clock.getElapsedTime() * 1) * 500);
+        //collideApi.velocity.set(0, 0, 0);.
+        missilesApi.position.set(
+          ...collideRef.current.getWorldPosition(BS.current.geometry.boundingSphere.center).add(new Vector3(0, 500, 0))
+        );
+        BS.current.material.color.set("blue");
       }
-    } else {
-      //collideApi.velocity.set(0, 0, Math.sin(clock.getElapsedTime() * 1) * 500);
-      collideApi.velocity.set(0, 0, 0);
-      move.current.material.color.set("blue");
     }
   });
+
   console.log("적 비행기");
   return (
     <group>
       <group ref={collideRef} dispose={null}>
         <axesHelper scale={1500} />
         <mesh ref={move}>
-          <sphereGeometry args={[args + 500]} />
-          <meshStandardMaterial wireframe opacity={0.5} transparent color={"green"} />
+          <sphereGeometry args={[args + 100]} />
+          <meshStandardMaterial wireframe opacity={0.1} transparent />
         </mesh>
         <mesh ref={BS}>
-          <sphereGeometry args={[args + 500]} />
-          <meshStandardMaterial wireframe opacity={0} transparent />
+          <sphereGeometry args={[args + 2500]} />
+          <meshStandardMaterial wireframe opacity={0.2} transparent />
         </mesh>
         <group rotation={[-Math.PI / 2, 0, 0]} scale={100}>
           <group rotation={[Math.PI / 2, 0, 0]}>
@@ -117,7 +138,18 @@ export const EnemyFighter = ({ args, position, mPos, rotation, num }) => {
         </group>
       </group>
 
-      {/* <MemoExplosive controll={missilesRef} api={missilesApi} pos={mPos} /> */}
+      <group ref={missileRef} dispose={null}>
+        <group position={[-200, 0, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+          <group position={[387.34, -175.97, 15.53]} rotation={[1.47, 0.76, 0.07]} />
+          <mesh geometry={missileModel.nodes.Material2.geometry} material={missileModel.materials.auto_13} />
+          <mesh geometry={missileModel.nodes.Material2_1.geometry} material={missileModel.materials.auto_14} />
+          <mesh geometry={missileModel.nodes.Material2_2.geometry} material={missileModel.materials.auto_16} />
+          <lineSegments
+            geometry={missileModel.nodes.Material2_3.geometry}
+            material={missileModel.nodes.Material2_3.material}
+          />
+        </group>
+      </group>
     </group>
   );
 };
