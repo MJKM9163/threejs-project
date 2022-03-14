b129445089601b3cb3f92cf92334a8a0a971d17a
rhkrrbaud <rhkrrbaudgg@naver.com>
2022-03-15 06:56:39 +0900

    행성 이름 랜덤 함수

--- src/hooks/planetNameSelect.js
+++ src/hooks/planetNameSelect.js
@@ -1,8 +1,17 @@
 import { useRef } from "react";
 import { planetStore } from "./stores/planetStore";
 
+let exc = [];
+
 export const PlanetNameSelect = () => {
   const planetName = useRef(planetStore.getState().planetName);
-  const randNum = Math.floor(Math.random() * planetName.current.length);
-  return planetName.current[randNum];
+  for (let i = 0; i < 1; i++) {
+    const randNum = Math.floor(Math.random() * planetName.current.length);
+    if (exc.includes(planetName.current[randNum])) {
+      i--;
+    } else {
+      exc.push(planetName.current[randNum]);
+      return planetName.current[randNum];
+    }
+  }
 };
