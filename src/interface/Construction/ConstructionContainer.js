431523f3648f79b8962093843f8d50157cc4e17a
rhkrrbaud <rhkrrbaudgg@naver.com>
2022-03-16 08:29:02 +0900

    건설 창 컴포넌트

--- src/interface/Construction/ConstructionContainer.js
+++ src/interface/Construction/ConstructionContainer.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import { screenStore } from "../../hooks/stores/screenStore";
 import { MemoProductionControl } from "./ProductionControl";
@@ -26,149 +26,221 @@ const HoverInfoContainer = styled.div`
     font-size: 20px;
   }
 
-  .imageScript {
-    font-size: 15px;
-  }
+  .imageBox {
+    display: flex;
+    flex-direction: column;
+    .image {
+      position: absolute;
+      display: block;
+      width: 450px;
+      height: 253px;
+    }
+    .imageScript {
+      position: absolute;
+      top: 285px;
+      font-size: 15px;
+    }
+    .cost-add {
+      padding-top: 10px;
+      padding-bottom: 5px;
+      border-bottom: 1px solid #b8b8b87a;
 
-  .sprite {
-    background-image: url("images/RRC.png");
-    background-repeat: no-repeat;
-    display: block;
-    width: 450px;
-    height: 253px;
-  }
+      .costTitle {
+        color: #dddddd;
+        width: 100%;
+        border-top: 1px solid #b8b8b87a;
+        font-size: 20px;
+      }
 
-  .sprite-adjustingTheAtmosphere {
-    background-position: -5px -5px;
-  }
+      .costText {
+        font-size: 18px;
+        margin-left: 5px;
+        font-weight: bold;
 
-  .sprite-biologicalDetector {
-    background-position: -465px -5px;
-  }
+        img {
+          margin-right: 5px;
+          vertical-align: top;
+        }
+      }
+      .cF {
+        color: DarkGreen;
+      }
+      .cT {
+        color: #1c58ff;
+      }
+      .cO {
+        color: #ff6a7d;
+      }
 
-  .sprite-combinationOfAtmosphericElements {
-    background-position: -925px -5px;
-  }
-
-  .sprite-defenseSatellite {
-    background-position: -5px -268px;
-  }
-
-  .sprite-dustExtractor {
-    background-position: -465px -268px;
-  }
-
-  .sprite-exploration {
-    background-position: -925px -268px;
-  }
-
-  .sprite-explorationOfSpace {
-    background-position: -5px -531px;
-  }
-
-  .sprite-friendlyGeneticManipulation {
-    background-position: -465px -531px;
-  }
-
-  .sprite-gear {
-    background-position: -925px -531px;
-  }
-
-  .sprite-magneticFieldGenerator {
-    background-position: -5px -794px;
-  }
-
-  .sprite-multipurposeSatellite {
-    background-position: -465px -794px;
-  }
-
-  .sprite-orichalcon {
-    background-position: -925px -794px;
-  }
-
-  .sprite-planetCurtain {
-    background-position: -5px -1057px;
-  }
-
-  .sprite-planetResearchInstitute {
-    background-position: -465px -1057px;
-  }
-
-  .sprite-planetTourismFacilities {
-    background-position: -925px -1057px;
-  }
-
-  .sprite-planetTrade {
-    background-position: -5px -1320px;
-  }
-
-  .sprite-potato {
-    background-position: -465px -1320px;
-  }
-
-  .sprite-reactorCatalyst {
-    background-position: -925px -1320px;
-  }
-
-  .sprite-science {
-    background-position: -1385px -5px;
-  }
-
-  .sprite-titanium {
-    background-position: -1385px -268px;
-  }
-
-  .sprite-titaniumAlloy {
-    background-position: -1385px -531px;
+      .addTitle {
+        margin-top: 5px;
+        color: #d3d3d3;
+      }
+      .addText {
+        margin: 2px 0px 0px 5px;
+        img {
+          margin-right: 5px;
+          vertical-align: bottom;
+        }
+      }
+      .aF {
+        color: #ff5757;
+      }
+      .aG {
+      }
+      .aS {
+      }
+      .aH {
+      }
+      .aB {
+      }
+      .aT {
+      }
+      .aO {
+      }
+    }
   }
 `;
 
 export const ConstructionContainer = () => {
-  let allData = screenStore.getState();
+  const resourcesProduction = screenStore.getState().resourcesProduction;
   const hoverCheck = screenStore((state) => state.hoverCheck);
-  //const hoverCheck = screenStore.getState().hoverCheck;
-  //const imagesMemo = useMemo(() => url("images/RRC.png"),[])
-  //const [hover, setHover] = useState(false);
-  //const resources = planetStore((state) => state.planetResources);
-
-  // useEffect(() => {
-  //   screenStore.subscribe(
-  //     (state) => state,
-  //     (state) => {
-  //       allData = state;
-  //     }
-  //   );
-  // }, []);
-  console.log(hoverCheck);
   console.log("행성 관리창 랜더링");
   return (
     <>
-      {/* {hoverCheck ? ( */}
       <HoverInfoContainer hoverCheck={hoverCheck !== false ? true : false}>
-        <div className="imageName">{allData.resourcesProduction[hoverCheck === false ? "potato" : hoverCheck].name}</div>
-        <div className={`${"sprite sprite-" + hoverCheck}`}></div>
-        <img
-          className="imageBox"
-          //width={450}
-          //height={250}
-          src={allData.resourcesProduction["potato"].img}
-          alt={"이미지"}></img>
-        {/* {allData[hoverCheck[0]][hoverCheck[1]].video ? (
-            <video muted loop autoPlay width={250} height={250} src={allData[hoverCheck[0]][hoverCheck[1]].video}></video>
-          ) : (
+        <div className="imageName">{resourcesProduction[hoverCheck === false ? "potato" : hoverCheck].name}</div>
+        {Object.keys(resourcesProduction).map((item, index) => (
+          <div key={item + index + " 박스"} className="imageBox">
             <img
-              className="imageBox"
-              //width={450}
-              //height={250}
-              src={allData[hoverCheck[0]][hoverCheck[1]].img}
-              alt={"이미지"}></img>
-          )} */}
-
-        <div className="imageScript">
-          {allData.resourcesProduction[hoverCheck === false ? "potato" : hoverCheck].description}
-        </div>
+              key={item + index + " 이미지"}
+              className="image"
+              src={resourcesProduction[item].img}
+              alt={`${item + " 이미지"}`}
+              style={hoverCheck === item ? { opacity: "1", zIndex: "100", display: "block" } : { display: "none" }}></img>
+            <div
+              key={item + index + " 설명"}
+              className="imageScript"
+              style={hoverCheck === item ? { opacity: "1", zIndex: "100", display: "block" } : { display: "none" }}>
+              {resourcesProduction[item].description}
+              {resourcesProduction[item].cost !== undefined ? (
+                <div className="cost-add">
+                  <div className="costTitle">비용</div>
+                  <span className="costText cF">
+                    <img src="images/resources/icons/corn.png" width={25} height={25} alt={`${item + " 식량 이미지"}`}></img>
+                    {resourcesProduction[item].cost.food}
+                  </span>
+                  {resourcesProduction[item].cost.titanium > 0 ? (
+                    <span className="costText cT">
+                      <img
+                        src="images/resources/icons/titanium.png"
+                        width={25}
+                        height={25}
+                        alt={`${item + " 티타늄 이미지"}`}></img>
+                      {resourcesProduction[item].cost.titanium}
+                    </span>
+                  ) : null}
+                  {resourcesProduction[item].cost.orichalcon > 0 ? (
+                    <span className="costText cO">
+                      <img
+                        src="images/resources/icons/orichalcon.png"
+                        width={25}
+                        height={25}
+                        alt={`${item + " 오리하르콘 이미지"}`}></img>
+                      {resourcesProduction[item].cost.orichalcon}
+                    </span>
+                  ) : null}
+                  {resourcesProduction[item].add !== undefined ? <div className="addTitle">효과</div> : null}
+                  {resourcesProduction[item].add.food !== undefined ? (
+                    <div
+                      className="addText aF"
+                      style={resourcesProduction[item].add.food > 0 ? { color: "#82ff82" } : { color: "#ff5757" }}>
+                      <img
+                        src="images/resources/icons/corn.png"
+                        width={20}
+                        height={20}
+                        alt={`${item + " 초당 식량 이미지"}`}></img>
+                      {resourcesProduction[item].add.food} /s
+                    </div>
+                  ) : null}
+                  {resourcesProduction[item].add.gear !== undefined ? (
+                    <div
+                      className="addText aG"
+                      style={resourcesProduction[item].add.gear > 0 ? { color: "#ffd29a" } : { color: "#ff5757" }}>
+                      <img
+                        src="images/resources/icons/gear.png"
+                        width={20}
+                        height={20}
+                        alt={`${item + " 생산력 이미지"}`}></img>
+                      {resourcesProduction[item].add.gear}
+                    </div>
+                  ) : null}
+                  {resourcesProduction[item].add.science !== undefined ? (
+                    <div
+                      className="addText aS"
+                      style={resourcesProduction[item].add.science > 0 ? { color: "#5fc4ff" } : { color: "#ff5757" }}>
+                      <img
+                        src="images/resources/icons/science.png"
+                        width={20}
+                        height={20}
+                        alt={`${item + " 과학 이미지"}`}></img>
+                      {resourcesProduction[item].add.science}
+                    </div>
+                  ) : null}
+                  {resourcesProduction[item].add.happiness !== undefined ? (
+                    <div
+                      className="addText aH"
+                      style={resourcesProduction[item].add.happiness > 0 ? { color: "#f1ffb4" } : { color: "#ff5757" }}>
+                      <img
+                        src="images/resources/icons/happiness.png"
+                        width={18}
+                        height={18}
+                        alt={`${item + " 행복도 이미지"}`}></img>
+                      {resourcesProduction[item].add.happiness}
+                    </div>
+                  ) : null}
+                  {resourcesProduction[item].add.barrier !== undefined ? (
+                    <div
+                      className="addText aB"
+                      style={resourcesProduction[item].add.barrier > 0 ? { color: "#af68ff" } : { color: "#ff5757" }}>
+                      <img
+                        src="images/resources/icons/barrier.png"
+                        width={20}
+                        height={20}
+                        alt={`${item + " 방어막 이미지"}`}></img>
+                      {resourcesProduction[item].add.barrier}
+                    </div>
+                  ) : null}
+                  {resourcesProduction[item].add.titanium !== undefined ? (
+                    <div
+                      className="addText aT"
+                      style={resourcesProduction[item].add.titanium > 0 ? { color: "#1c58ff" } : { color: "#ff5757" }}>
+                      <img
+                        src="images/resources/icons/titanium.png"
+                        width={20}
+                        height={20}
+                        alt={`${item + " 티타늄 이미지"}`}></img>
+                      {resourcesProduction[item].add.titanium} /s
+                    </div>
+                  ) : null}
+                  {resourcesProduction[item].add.orichalcon !== undefined ? (
+                    <div
+                      className="addText aO"
+                      style={resourcesProduction[item].add.orichalcon > 0 ? { color: "#ff6a7d" } : { color: "#ff5757" }}>
+                      <img
+                        src="images/resources/icons/orichalcon.png"
+                        width={20}
+                        height={20}
+                        alt={`${item + " 오리하르콘 이미지"}`}></img>
+                      {resourcesProduction[item].add.orichalcon} /s
+                    </div>
+                  ) : null}
+                </div>
+              ) : null}
+            </div>
+          </div>
+        ))}
       </HoverInfoContainer>
-      {/* ) : null} */}
       <MemoProductionControl />
     </>
   );
