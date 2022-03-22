83b8e0101a8a97f836414212022662bcc693ae15
rhkrrbaud <rhkrrbaudgg@naver.com>
2022-03-22 12:03:35 +0900

    아군 비행체 컴포넌트

--- src/flyingObjects/Friendlys.js
+++ src/flyingObjects/Friendlys.js
@@ -6,20 +6,32 @@ export const Friendlys = () => {
   const num = useRef();
   //const friendlyNum = boundingStore.getState().friendlyNum;
   const friendlyNum = boundingStore((state) => state.friendlyNum);
+  console.log(friendlyNum);
 
   return (
     <group ref={num}>
-      {friendlyNum.map((item, index) => (
-        <BasicFighter
-          key={index}
-          num={index + 1}
-          position={[-2500, 0, -3000 + index * -800]}
-          mPos={[-2500, 500, -3000 + index * -800]}
-          rotation={[0, 0, 0]}
-          args={300}
-          scale={25}
-        />
-      ))}
+      {friendlyNum.map((item, index) =>
+        item === true ? (
+          <BasicFighter
+            key={index}
+            num={index}
+            position={[-2500, 0, -3000 + index * -800]}
+            rotation={[0, 0, 0]}
+            args={0}
+            scale={25}
+          />
+        ) : null
+      )}
     </group>
   );
 };
+
+// event: () => {
+//   let num = boundingStore.getState().friendlyNum.findIndex((e) => e === false);
+//   console.log(boundingStore.getState().friendlyNum[num]);
+//   let data = (boundingStore.getState().friendlyNum[num] = true);
+
+//   console.log(num);
+//   console.log(data);
+//   boundingStore.setState({ friendlyNum: boundingStore.getState().friendlyNum });
+// },
