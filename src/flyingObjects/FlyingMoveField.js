13dfa766dcfe8c3c5deae2d002366f6aee62ef29
rhkrrbaud <rhkrrbaudgg@naver.com>
2022-03-25 17:23:40 +0900

    비행기 이동 필드 컴포넌트

--- src/flyingObjects/FlyingMoveField.js
+++ src/flyingObjects/FlyingMoveField.js
@@ -4,26 +4,24 @@ import { screenStore } from "../hooks/stores/screenStore";
 import { TextureLoader } from "three";
 
 export const FlyingMoveField = () => {
-  const field = new Array(1).fill(0);
   const flyingMoveMapCheck = screenStore((state) => state.flyingMoveMapCheck);
   const moveMap1 = useMemo(() => new TextureLoader().load(moveMap), []);
+
   return (
     <group>
-      {flyingMoveMapCheck === true ? (
-        <group>
-          {field.map((zitem, zindex) => (
-            <mesh
-              key={"z" + zindex + "f"}
-              position={[0, -10, 0]}
-              rotation={[Math.PI / 0.66666, 0, 0]}
-              onClick={(e) => {
-                screenStore.setState({ flyingMovePos: e.point });
-              }}>
-              <planeGeometry args={[15000, 15000]} />
-              <meshBasicMaterial color="#2d6279" map={moveMap1} attach="material" />
-            </mesh>
-          ))}
-        </group>
+      {flyingMoveMapCheck !== null ? (
+        <mesh
+          position={[0, -10, 0]}
+          rotation={[Math.PI / 0.66666, 0, 0]}
+          onClick={(e) => {
+            console.log("클릭");
+            const data = screenStore.getState().flyingMovePos;
+            data[flyingMoveMapCheck] = e.point;
+            screenStore.setState({ flyingMovePos: data });
+          }}>
+          <planeGeometry args={[15000, 15000]} />
+          <meshBasicMaterial color="#2d6279" map={moveMap1} attach="material" />
+        </mesh>
       ) : null}
     </group>
   );
