a5acc0239527e9e3dd0c0dd971feacce35af2408
rhkrrbaud <rhkrrbaudgg@naver.com>
2022-03-30 23:27:05 +0900

    이벤트 코드 수정

--- src/hooks/EventBox.js
+++ src/hooks/EventBox.js
@@ -1,5 +1,6 @@
-import React, { useEffect } from "react";
+import React from "react";
 import styled from "styled-components";
+import { effectSound } from "./stores/effectSound";
 import { eventStore } from "./stores/eventStore";
 import { screenStore } from "./stores/screenStore";
 
@@ -10,7 +11,7 @@ const EventContainer = styled.div`
   flex-direction: column;
   width: 850px;
   height: 600px;
-  top: 50%;
+  top: 45%;
   left: 50%;
   transform: translate(-50%, -50%);
   transition: 0.5s opacity;
@@ -74,32 +75,110 @@ const EventContainer = styled.div`
   }
 `;
 
-export const EventBox = () => {
+const Event = () => {
   const eventCheck = screenStore((state) => state.eventCheck);
   const preEvent = eventStore((state) => state.preEvent);
-
-  console.log(preEvent);
   return (
-    <EventContainer check={eventCheck}>
-      <div className="eventName">{preEvent.name}</div>
+    <div>
+      <EventContainer check={eventCheck}>
+        <div className="eventName">{preEvent.name}</div>
+        <div className="eventImage">
+          <img src={preEvent.img} alt="eventImg" />
+        </div>
+        <div className="eventDescription">{preEvent.description}</div>
+        <div className="eventButton">
+          <span
+            onClick={() => {
+              preEvent.assent();
+            }}>
+            {preEvent.assentText}
+          </span>
+          <span
+            onClick={() => {
+              preEvent.dissent();
+            }}>
+            {preEvent.dissentText}
+          </span>
+        </div>
+      </EventContainer>
+    </div>
+  );
+};
+
+const ExEvent = () => {
+  const exCheck = screenStore((state) => state.exCheck);
+  const [eventSelect, eventName] = eventStore.getState().eventR("ex");
+  if (exCheck === true) {
+    effectSound.getState().event.exEvent.action();
+  }
+
+  return (
+    <EventContainer check={exCheck}>
+      <div className="eventName">{eventSelect.name}</div>
       <div className="eventImage">
-        <img src={preEvent.img} alt="eventImg" />
+        <img src={eventSelect.img} alt="eventImg" />
       </div>
-      <div className="eventDescription">{preEvent.description}</div>
+      <div className="eventDescription">{eventSelect.description}</div>
       <div className="eventButton">
         <span
           onClick={() => {
-            preEvent.assent();
+            eventSelect.assent();
+            eventStore.setState((set) => (set.exEventList[eventName].check = true));
           }}>
-          {preEvent.assentText}
+          {eventSelect.assentText}
         </span>
         <span
           onClick={() => {
-            preEvent.dissent();
+            eventSelect.dissent();
+            eventStore.setState((set) => (set.exEventList[eventName].check = true));
           }}>
-          {preEvent.dissentText}
+          {eventSelect.dissentText}
         </span>
       </div>
     </EventContainer>
+  );
+};
+
+const SExEvent = () => {
+  const sExCheck = screenStore((state) => state.sExCheck);
+  const [eventSelect, eventName] = eventStore.getState().eventR("sEx");
+  if (sExCheck === true) {
+    effectSound.getState().event.sExEvent.action();
+  }
+
+  return (
+    <EventContainer check={sExCheck}>
+      <div className="eventName">{eventSelect.name}</div>
+      <div className="eventImage">
+        <img src={eventSelect.img} alt="eventImg" />
+      </div>
+      <div className="eventDescription">{eventSelect.description}</div>
+      <div className="eventButton">
+        <span
+          onClick={() => {
+            eventSelect.assent();
+            eventStore.setState((set) => (set.sExEventList[eventName].check = true));
+          }}>
+          {eventSelect.assentText}
+        </span>
+        <span
+          onClick={() => {
+            eventSelect.dissent();
+            eventStore.setState((set) => (set.sExEventList[eventName].check = true));
+          }}>
+          {eventSelect.dissentText}
+        </span>
+      </div>
+    </EventContainer>
+  );
+};
+
+export const EventBox = () => {
+  return (
+    <div>
+      <Event />
+      <ExEvent />
+      <SExEvent />
+    </div>
   );
 };
--- src/hooks/clockEvents.js
+++ src/hooks/clockEvents.js
@@ -1,5 +1,6 @@
 import { useFrame } from "@react-three/fiber";
 import { useEffect, useRef } from "react";
+import { effectSound } from "./stores/effectSound";
 import { eventStore } from "./stores/eventStore";
 import { screenStore } from "./stores/screenStore";
 
@@ -18,15 +19,16 @@ export const ClockEvents = () => {
 
   useFrame(({ clock }) => {
     if (eventCheck.current === false && clock.elapsedTime >= 300 * clockN * clockR) {
-      const [eventSelect, eventName] = eventStore.getState().eventR();
+      const [eventSelect, eventName] = eventStore.getState().eventR("e");
       if (eventSelect !== null) {
         clockN++;
         clockR = Number((Math.random() * (1.5 - 0.8) + 0.8).toFixed(2));
         eventStore.setState({ preEvent: eventSelect });
         eventStore.setState((set) => (set.eventList[eventName].check = true));
         screenStore.setState({ eventCheck: true });
+        effectSound.getState().event.basicEvent.action();
       } else {
-        clockN = 100000;
+        clockN = 500000;
       }
     }
   });
