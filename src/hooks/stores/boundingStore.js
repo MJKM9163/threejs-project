eb3a394a4a93b6d44df650a22c8070aad3195cbf
rhkrrbaud <rhkrrbaudgg@naver.com>
2022-04-01 23:45:43 +0900

    전투기, 무기 정보 추가

--- src/hooks/stores/boundingStore.js
+++ src/hooks/stores/boundingStore.js
@@ -9,11 +9,22 @@ export const boundingStore = create(
       friendly: {},
       enemy: {},
     },
-
+    // 무기 정보
+    weapons: {
+      missile: { type: "missile", damage: 20 },
+      pulse: { type: "pulse", damage: 10 },
+    },
     // 적군 정보 max 12
-    enemyNum: [false, false, false, false, false, false, false, false, false, false, false, false],
+    enemyData: {
+      basic: { type: "공격함", durability: 100, adjust: 1 },
+      battleship: { type: "전함", durability: 500, adjust: 1.5 },
+    },
+    enemyLive: [false, false, false, false, false, false, false, false, false, false, false, false],
     // 아군 정보 max 3
-    friendlyNum: [false, false, false],
+    friendlyData: {
+      basic: { type: "공격함", durability: 200, adjust: 1.1 },
+      battleship: { type: "전함", durability: 1200, adjust: 1.8 },
+    },
+    friendlyLive: [false, false, false],
   }))
 );
-//{ MPos: new Vector3(0, 2000, 0), TPos: new Vector3(1000, 0, 0) }
