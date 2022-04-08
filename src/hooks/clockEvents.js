11c5f54259877b8433588bac6eb55157f6b9e3c0
rhkrrbaud <rhkrrbaudgg@naver.com>
2022-04-08 16:11:46 +0900

    시간 이벤트 비활성

--- src/hooks/clockEvents.js
+++ src/hooks/clockEvents.js
@@ -8,30 +8,30 @@ let clockN = 1;
 let clockR = Number((Math.random() * (1.5 - 0.8) + 0.8).toFixed(2));
 
 export const ClockEvents = () => {
-  const eventCheck = useRef(screenStore.getState().eventCheck);
+  // const eventCheck = useRef(screenStore.getState().eventCheck);
 
-  useEffect(() => {
-    screenStore.subscribe(
-      (state) => (eventCheck.current = state.eventCheck),
-      (state) => state
-    );
-  });
+  // useEffect(() => {
+  //   screenStore.subscribe(
+  //     (state) => (eventCheck.current = state.eventCheck),
+  //     (state) => state
+  //   );
+  // });
 
-  useFrame(({ clock }) => {
-    if (eventCheck.current === false && clock.elapsedTime >= 300 * clockN * clockR) {
-      const [eventSelect, eventName] = eventStore.getState().eventR("e");
-      if (eventSelect !== null && eventName !== null) {
-        clockN++;
-        clockR = Number((Math.random() * (1.5 - 0.8) + 0.8).toFixed(2));
-        eventStore.setState({ preEvent: eventSelect });
-        eventStore.setState((set) => (set.eventList[eventName].check = true));
-        screenStore.setState({ eventCheck: true });
-        effectSound.getState().event.basicEvent.action();
-      } else {
-        clockN = 500000;
-      }
-    }
-  });
+  // useFrame(({ clock }) => {
+  //   if (eventCheck.current === false && clock.elapsedTime >= 300 * clockN * clockR) {
+  //     const [eventSelect, eventName] = eventStore.getState().eventR("e");
+  //     if (eventSelect !== null && eventName !== null) {
+  //       clockN++;
+  //       clockR = Number((Math.random() * (1.5 - 0.8) + 0.8).toFixed(2));
+  //       eventStore.setState({ preEvent: eventSelect });
+  //       eventStore.setState((set) => (set.eventList[eventName].check = true));
+  //       screenStore.setState({ eventCheck: true });
+  //       effectSound.getState().event.basicEvent.action();
+  //     } else {
+  //       clockN = 500000;
+  //     }
+  //   }
+  // });
 
   return null;
 };
