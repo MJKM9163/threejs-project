b5f3f6efb6274a76e54fb573c74daf072f86eee6
rhkrrbaud <rhkrrbaudgg@naver.com>
2022-04-11 22:15:14 +0900

    행성 효과 text 추가, 수정

--- src/hooks/stores/effectStore.js
+++ src/hooks/stores/effectStore.js
@@ -35,21 +35,30 @@ export const effectStore = create(
         name: "포자 지대",
         img: "/images/effects/sporeZone.jpg",
         resources: { food: -0.2 },
-        description: "포자",
+        description:
+          "독성을 내뿜는 포자가 가득한 영역이 존재합니다. 이 영역은 조용하고 평화롭게 보이지만 가까이 접근하게 된다면 원인을 알기도 전에 몸이 가루로 변할 것입니다.",
       },
       {
         name: "적대적 환경",
         img: "/images/effects/hostileEnvironment.jpg",
-        resources: { science: -2, happiness: -3 },
-        description: "적대적",
+        resources: { science: -2, happiness: -7 },
+        description:
+          "이 행성에는 적대적 생물이 가득합니다. 쉽게 제압이 가능하지만 언제 어디서 나타날지 모르기 때문에 항상 긴장하며 생활해야 합니다.",
       },
       {
         name: "불길한 흔적",
         img: "/images/effects/ominousTrace.jpg",
         resources: { science: 3, happiness: -8 },
-        description: "불길한",
+        description:
+          "행성 표면에 인공적으로 뚫린 원형 구멍이 곳곳에 있습니다. 이유는 알 수 없지만 자연적인 현상이 아닌 것만으로 충분히 위협적이고 흥미롭습니다.",
       },
-      { name: "벌레 무리", img: "/images/effects/swarm.jpg", resources: { gear: -2 }, description: "벌레" },
+      {
+        name: "벌레 무리",
+        img: "/images/effects/swarm.jpg",
+        resources: { gear: -2, happiness: -3 },
+        description:
+          "불쾌한 벌레들이 무리를 지어 행동하고 있습니다. 생각보다 높은 지능을 가진 이 벌레들은 거주민들에게 혐오감과 공포를 줍니다.",
+      },
     ],
     positiveEffectModels: { 나노공장: <NanoFactory />, 아름다운고리: <Ring /> },
   }))
