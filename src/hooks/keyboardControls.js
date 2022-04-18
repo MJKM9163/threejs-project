9021b71872f0a37d7ecf53f5be87cb9316a7f411
rhkrrbaud <rhkrrbaudgg@naver.com>
2022-04-18 23:06:12 +0900

    단축키 추가

--- src/hooks/keyboardControls.js
+++ src/hooks/keyboardControls.js
@@ -1,4 +1,7 @@
 import { useEffect, useState } from "react";
+import { screenStore } from "./stores/screenStore";
+
+let prevention = true;
 
 function actionByKey(key) {
   const keys = {
@@ -29,11 +32,30 @@ export const KeyboardControls = () => {
         setMovement((state) => ({ ...state, [actionByKey(e.code)]: false }));
       }
     };
+    const hotKeys = (e) => {
+      if (prevention) {
+        prevention = false;
+        if (e.code === "Digit1") {
+          const check = screenStore.getState().researchMapOnOff;
+          screenStore.setState({ researchMapOnOff: !check });
+        } else if (e.code === "Digit2") {
+          const check = screenStore.getState().productionControl;
+          screenStore.setState({ productionControl: !check });
+        }
+      }
+    };
+    const hotKeysUp = () => {
+      prevention = true;
+    };
     document.addEventListener("keydown", handleKeyDown);
     document.addEventListener("keyup", handleKeyUp);
+    document.addEventListener("keydown", hotKeys);
+    document.addEventListener("keyup", hotKeysUp);
     return () => {
       document.addEventListener("keydown", handleKeyDown);
       document.addEventListener("keyup", handleKeyUp);
+      document.addEventListener("keydown", hotKeys);
+      document.addEventListener("keyup", hotKeysUp);
     };
   });
   return movement;
